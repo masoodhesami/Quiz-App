ea77163a105858f1585095957f9925e3a722b7d8
Masoud Hesami <masoodhesami79@gmail.com>
2022-01-16 12:56:23 +0330

    quiz menu created

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ import {quizContext} from "./helpers/contexts"
 
 function App() {
     const [navigator, setNavigator] = useState("menu")
+    const [username, setUsername] = useState("")
     return (
         <div className="App">
-            <quizContext.Provider value={{navigator, setNavigator}}>
-                {navigator === "menu" && <MainMenu/>}
-                {navigator === "quiz" && <Quiz/>}
-                {navigator === "result" && <Result/>}
-            </quizContext.Provider>
+            <div className="card">
+                <quizContext.Provider value={{navigator, setNavigator}}>
+                    <div className="card-body">
+                        {navigator === "menu" && <MainMenu/>}
+                        {navigator === "quiz" && <Quiz/>}
+                        {navigator === "result" && <Result/>}
+                    </div>
+                </quizContext.Provider>
+            </div>
         </div>
     );
 }
--- src/components/mainMenu.jsx
+++ src/components/mainMenu.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {quizContext} from "../helpers/contexts";
+import "../App.css"
 
 const MainMenu = () => {
+    const {setNavigator, setUsername} = useContext(quizContext)
     return (
-        <div>
-            menu
+        <div className={"row d-flex mt-5"}>
+        <div className={"container"}>
+            <div className={"row mt-2"}>
+                <h2 className={"text-primary"}>
+                    Welcome to React Quiz Application
+                </h2>
+            </div>
+            <div className={"row mx-auto d-flex justify-content-center mt-5"}>
+                <div className={"col-4"}>
+                    <form>
+                        <div className="form-group">
+                            <input type="email" className="form-control" id="exampleInputEmail1"
+                                   aria-describedby="emailHelp"
+                                   placeholder="Enter Your Name "
+                                   onChange={(event) => {
+                                       setUsername(event.target.value)
+                                   }
+                                   }/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className={"row mt-4 d-flex justify-content-center"}>
+                <div className={"col-2"}>
+                    <div className={"row"}>
+                    <button onClick={() => {
+                        setNavigator("quiz")
+                    }} type="button" className="btn btn-outline-primary ">Start Quiz
+                    </button>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
